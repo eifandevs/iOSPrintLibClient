2d5cafc07c7a81bdf691ce0189c10e2480c804d7
temma.io <eifan.devs@gmail.com>
2020-10-08 14:16:03 +0900

    add file

---
+++ iOSPrintLibClient/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSPrintLibClient
+//
+//  Created by iori tenma on 2020/10/08.
+//  Copyright © 2020 iori tenma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
